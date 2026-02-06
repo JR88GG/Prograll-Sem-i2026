2f9d779ff46baa1ffab8cc4421448566feace4db
Herber Marin <ccjunior48@gmail.com>
2026-02-06 09:51:08 -0600

    creación de rama conversores

--- miPrimerApp2/app/src/main/java/com/example/miprimerapp2/MainActivity.java
+++ miPrimerApp2/app/src/main/java/com/example/miprimerapp2/MainActivity.java
@@ -13,45 +13,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;//variable con valor temporal
-    Button btn;//
-    Spinner spn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v -> calcular());
+
     }
 
-    private void calcular() {
-        tempVal = findViewById(R.id.txtNum1);
-        double num1 = Double.parseDouble(tempVal.getText().toString());
 
-        tempVal = findViewById(R.id.txtNum2);
-        double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 0;
-
-        spn = findViewById(R.id.cboOpciones);
-        switch (spn.getSelectedItemPosition()){
-            case 0: //suma
-                respuesta = num1 + num2;
-                break;
-            case 1: //Resta
-                respuesta = num1 - num2;
-                break;
-            case 2: //Multiplicacion
-                respuesta = num1 * num2;
-                break;
-            case 3: //division
-                respuesta = num1 / num2;
-                break;
-        }
-            //agregar factorial, exponenciacion, porcentaje, raiz
-            tempVal = findViewById(R.id.lblRespuesta);
-            tempVal.setText("Respuesta: " + respuesta);
-        }
     }
