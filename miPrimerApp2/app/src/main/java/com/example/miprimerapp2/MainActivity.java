70e333b20e40aaddfa20fdc16902e77f9df95502
Herber Marin <ccjunior48@gmail.com>
2026-02-06 09:43:50 -0600

    Opciones de operación de numeros con spiner(combobox) y con switch poniendo todo mas ordenado dentro de una selección de casos

--- miPrimerApp2/app/src/main/java/com/example/miprimerapp2/MainActivity.java
+++ miPrimerApp2/app/src/main/java/com/example/miprimerapp2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.miprimerapp2;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;//variable con valor temporal
     Button btn;//
+    Spinner spn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +23,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v->calcular());
+        btn.setOnClickListener(v -> calcular());
     }
-    private void calcular(){
+
+    private void calcular() {
         tempVal = findViewById(R.id.txtNum1);
         double num1 = Double.parseDouble(tempVal.getText().toString());
 
         tempVal = findViewById(R.id.txtNum2);
         double num2 = Double.parseDouble(tempVal.getText().toString());
 
-        double  respuesta = num1 + num2;
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta"+ respuesta);
+        double respuesta = 0;
+
+        spn = findViewById(R.id.cboOpciones);
+        switch (spn.getSelectedItemPosition()){
+            case 0: //suma
+                respuesta = num1 + num2;
+                break;
+            case 1: //Resta
+                respuesta = num1 - num2;
+                break;
+            case 2: //Multiplicacion
+                respuesta = num1 * num2;
+                break;
+            case 3: //division
+                respuesta = num1 / num2;
+                break;
+        }
+            //agregar factorial, exponenciacion, porcentaje, raiz
+            tempVal = findViewById(R.id.lblRespuesta);
+            tempVal.setText("Respuesta: " + respuesta);
+        }
     }
-}
