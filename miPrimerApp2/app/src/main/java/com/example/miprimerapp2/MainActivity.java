c7dc3d58e0f0bfa70fb23a5d0d1b1064cfdbab2c
Herber Marin <ccjunior48@gmail.com>
2026-02-13 09:00:29 -0600

    Se agrego los iconos a las pestañas y se programo el de longitud completo su funcionaliad

--- miPrimerApp2/app/src/main/java/com/example/miprimerapp2/MainActivity.java
+++ miPrimerApp2/app/src/main/java/com/example/miprimerapp2/MainActivity.java
@@ -19,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     Spinner spn;
     Button btn;
-    Double valores[] = new Double[] {1.0, 0.85, 7.67, 26.42, 36.80, 495.77};
+    Double monedas[] = new Double[] {1.0, 0.85, 7.67, 26.42, 36.80, 495.77};
+    Double longitud[] = new Double[] {1.0, 1000.0, 100.0, 39.3701, 3.280841666667, 1.1963081929167, 1.09361};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +31,30 @@ public class MainActivity extends AppCompatActivity {
         tbh = findViewById(R.id.tbhConversores);
         tbh.setup();
 
-        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
-        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
-        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabVolumen).setIndicator("VOLUMEN", null));
-        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabMasa).setIndicator("MASA", null));
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS",getResources().getDrawable(R.drawable.coin)));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD",getResources().getDrawable(R.drawable.lenght)));
+        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabVolumen).setIndicator("VOLUMEN", getResources().getDrawable(R.drawable.medida_de_volumen)));
+        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabMasa).setIndicator("MASA",getResources().getDrawable(R.drawable.balance) ));
 
         btn = findViewById(R.id.btnMonedasConvertir);
         btn.setOnClickListener(v->convertirMonedas());
+
+        btn = findViewById(R.id.btnLongitudConvertir);
+        btn.setOnClickListener(v->convertirLongitud());
+    }
+    private void convertirLongitud(){
+        spn = findViewById(R.id.spnLongitudDe);
+        int de = spn.getSelectedItemPosition();
+
+        spn = findViewById(R.id.spnLongitudA);
+        int a = spn.getSelectedItemPosition();
+
+        tempVal = findViewById(R.id.txtLongitudCantidad);
+        double cantidad = Double.parseDouble(tempVal.getText().toString());
+        double respuesta = conversorLongitud(de, a, cantidad);
+
+        tempVal = findViewById(R.id.lblLongitudRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
     }
     private void convertirMonedas(){
         spn = findViewById(R.id.spnMonedasDe);
@@ -52,9 +71,10 @@ public class MainActivity extends AppCompatActivity {
         tempVal.setText("Respuesta: "+ respuesta);
     }
     double conversor(int de, int a, double cantidad){
-        return valores[a]/valores[de] * cantidad;
-
+        return monedas[a]/monedas[de] * cantidad;
     }
-
+    double conversorLongitud(int de, int a, double cantidad){
+        return longitud[a]/longitud[de] * cantidad;
+    }
 
 }
